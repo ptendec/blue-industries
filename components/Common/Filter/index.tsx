6f6e56bcdc1c68a515294862067c2d1c2dee9ff5
an4morph <avgustin456@gmail.com>
2024-06-19 18:29:19 +0600

    fix tabs

--- components/Common/Filter/index.tsx
+++ components/Common/Filter/index.tsx
@@ -2,11 +2,12 @@ import { useDisclosure } from "@mantine/hooks";
 import { useEmployeeVisibilityStore } from "../../../store";
 import FilterAccordion from "../FilterAccordion";
 import Select from "../Select";
-import Tabs from "../Tabs";
 import styles from "./style.module.css";
 import { EyeIcon } from "../../SvgIcons/eye";
 import { EyeHideIcon } from "../../SvgIcons/eye-hide";
 import FilterTabs from "../FilterTabs";
+import SortSelect from "../SortSelect";
+import { BottomFilter } from "./bottom";
 
 export const Filter = () => {
   const [opened, { toggle }] = useDisclosure(false);
@@ -51,48 +52,10 @@ export const Filter = () => {
                   {value ? <EyeIcon /> : <EyeHideIcon />}
                 </div>
               ))}
-          </div>
+              <BottomFilter handleSelectChange={handleSelectChange}/>
+            </div>
         </FilterAccordion>
 
-        <div className={styles.actions}>
-          <Select
-            options={["Ascending grade", "Decreasing rating"]}
-            placeholder="Sort by ..."
-            onChange={handleSelectChange}
-          />
-          <button
-            style={{
-              background: "rgba(153, 213, 42, 1)",
-              color: "white",
-            }}
-            className={styles.button}
-          >
-            Best teams 😊
-          </button>
-          <button
-            style={{
-              background: "rgba(255, 178, 36, 1)",
-              color: "white",
-            }}
-            className={styles.button}
-          >
-            Average Teams 😐
-          </button>
-          <button
-            style={{
-              background: "rgba(229, 77, 46, 1)",
-              color: "white",
-            }}
-            className={styles.button}
-          >
-            Low Teams 😠
-          </button>
-          <Select
-            options={["Export to PDF", "Export to Excel"]}
-            placeholder="Export data"
-            onChange={handleSelectChange}
-          />
-        </div>
         </div>
       </div>
     </div>
