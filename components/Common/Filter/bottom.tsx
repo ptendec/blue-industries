572c7831650eb298ed95d3008a41c0af8cc94959
Sherkhan <sherkhan.iskender@kcell.kz>
2024-06-19 17:40:46 +0500

    fix: clickable

--- components/Common/Filter/bottom.tsx
+++ components/Common/Filter/bottom.tsx
@@ -1,53 +1,62 @@
-import { useDisclosure } from "@mantine/hooks";
 import { useEmployeeVisibilityStore } from "../../../store";
-import FilterAccordion from "../FilterAccordion";
 import Select from "../Select";
-import styles from "./style.module.css";
-import { EyeIcon } from "../../SvgIcons/eye";
-import { EyeHideIcon } from "../../SvgIcons/eye-hide";
-import FilterTabs from "../FilterTabs";
 import SortSelect from "../SortSelect";
+import styles from "./style.module.css";
 
 export const BottomFilter = ({ handleSelectChange }: any) => {
+  const { filterBy, setFilterBy } = useEmployeeVisibilityStore();
+
   return (
     <div className={styles.actions}>
-          <SortSelect
-            options={["Ascending grade", "Decreasing rating"]}
-            placeholder="Sort by ..."
-            onChange={handleSelectChange}
-          />
-          <button
-            style={{
-              background: "rgba(153, 213, 42, 1)",
-              color: "white",
-            }}
-            className={styles.button}
-          >
-            Best teams 😊
-          </button>
-          <button
-            style={{
-              background: "rgba(255, 178, 36, 1)",
-              color: "white",
-            }}
-            className={styles.button}
-          >
-            Average Teams 😐
-          </button>
-          <button
-            style={{
-              background: "rgba(229, 77, 46, 1)",
-              color: "white",
-            }}
-            className={styles.button}
-          >
-            Low Teams 😠
-          </button>
-          <Select
-            options={["Export to PDF", "Export to Excel"]}
-            placeholder="Export data"
-            onChange={handleSelectChange}
-          />
+      <SortSelect
+        options={["Ascending grade", "Decreasing rating"]}
+        placeholder="Sort by ..."
+        onChange={handleSelectChange}
+      />
+      <button
+        style={{
+          background: "rgba(153, 213, 42, 1)",
+          color: "white",
+          ...(filterBy === "Best" && {
+            border: "2px solid rgba(114, 157, 33, 1)",
+          }),
+        }}
+        onClick={() => setFilterBy(filterBy === "Best" ? "None" : "Best")}
+        className={styles.button}
+      >
+        Best teams 😊
+      </button>
+      <button
+        style={{
+          background: "rgba(255, 178, 36, 1)",
+          color: "white",
+          ...(filterBy === "Average" && {
+            border: "2px solid rgba(196, 137, 28, 1)",
+          }),
+        }}
+        className={styles.button}
+        onClick={() => setFilterBy(filterBy === "Average" ? "None" : "Average")}
+      >
+        Average Teams 😐
+      </button>
+      <button
+        style={{
+          background: "rgba(229, 77, 46, 1)",
+          color: "white",
+          ...(filterBy === "Low" && {
+            border: "2px solid rgba(168, 56, 33, 1)",
+          }),
+        }}
+        className={styles.button}
+        onClick={() => setFilterBy(filterBy === "Low" ? "None" : "Low")}
+      >
+        Low Teams 😠
+      </button>
+      <Select
+        options={["Export to PDF", "Export to Excel"]}
+        placeholder="Export data"
+        onChange={handleSelectChange}
+      />
     </div>
-  )
-}
+  );
+};
