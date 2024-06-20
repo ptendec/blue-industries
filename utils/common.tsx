84af0ed9e44f1d41d9c400d40a92e1ac1e7b4cf6
Sherkhan <sherkhan.iskender@kcell.kz>
2024-06-20 16:15:11 +0500

    fkx

--- utils/common.tsx
+++ utils/common.tsx
@@ -3,9 +3,10 @@ import { SmileGood } from "../components/SvgIcons/smile-good";
 import { SmileMedium } from "../components/SvgIcons/smile-medium";
 
 export const valueToEmoji = (value: number | string): JSX.Element | string => {
-  if (Number(value) === 3) return <SmileGood />;
-  else if (Number(value) === 2) return <SmileMedium />;
-  else if (Number(value) === 1) return <SmileBad />;
+  const val = Number(value);
+  if (val > 2.25 && val <= 3) return <SmileGood />;
+  else if (val > 1.75 && val <= 2.25) return <SmileMedium />;
+  else if (val <= 1.75) return <SmileBad />;
 
   return "";
 };
